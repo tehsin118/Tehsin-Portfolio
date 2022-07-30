023570168d96ecce0dc2a8e50a54f786ab231f98
tehsin118 <tehsin118@gmail.com>
2022-07-30 16:57:41 +0500

    portfolio

---
+++ script/style-switcher.js
@@ -0,0 +1,40 @@
+/* ------------------------ toggle style switcher  ------------------------*/
+
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+/* ------------------------ toggle style switcher  End------------------------*/
+
+const alternateStyle = document.querySelector(".alternate-style");
+// const alternateStyle = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+  alternateStyle.forEach((style) => {
+    if (color === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+/* ------------------------ Theme Mode------------------------*/
+const dayNight = document.querySelector(".day-night");
+
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").addEventListener("fa-sun");
+  } else {
+    dayNight.querySelector("i").addEventListener("fa-moon");
+  }
+});
