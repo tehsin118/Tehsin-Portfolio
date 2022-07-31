6ddfc8ecf531f529d2d82b611b84ba01f3e26582
tehsin118 <tehsin118@gmail.com>
2022-07-31 09:53:26 +0500

    Update AsideBar

--- script/style-switcher.js
+++ script/style-switcher.js
@@ -1,40 +1,16 @@
-/* ------------------------ toggle style switcher  ------------------------*/
-
-const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
-styleSwitcherToggle.addEventListener("click", () => {
-  document.querySelector(".style-switcher").classList.toggle("open");
-});
-window.addEventListener("scroll", () => {
-  if (document.querySelector(".style-switcher").classList.contains("open")) {
-    document.querySelector(".style-switcher").classList.remove("open");
-  }
-});
-/* ------------------------ toggle style switcher  End------------------------*/
-
-const alternateStyle = document.querySelector(".alternate-style");
-// const alternateStyle = document.querySelectorAll(".alternate-style");
-function setActiveStyle(color) {
-  alternateStyle.forEach((style) => {
-    if (color === style.getAttribute("title")) {
-      style.removeAttribute("disabled");
-    } else {
-      style.setAttribute("disabled", "true");
-    }
-  });
-}
 /* ------------------------ Theme Mode------------------------*/
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
+  dayNight.querySelector("i").classList.toggle("fa-sun");
   document.body.classList.toggle("dark");
 });
 
 window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").addEventListener("fa-sun");
+    dayNight.querySelector("i").addEventListener("fa-moon  ");
   } else {
-    dayNight.querySelector("i").addEventListener("fa-moon");
+    dayNight.querySelector("i").addEventListener("fa-sun");
   }
 });
